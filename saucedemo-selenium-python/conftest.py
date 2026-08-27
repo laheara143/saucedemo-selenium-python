8eda2d4152cf8055b16f0f7dd370a131e70dfa26
Hannah Cameron <77558582+laheara143@users.noreply.github.com>
2026-08-27 16:15:54 +0000

    Add setup framework for pytest/selenium and get demo script started

---
+++ saucedemo-selenium-python/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def driver():
+
+    options = webdriver.ChromeOptions()
+
+    driver = webdriver.Remote(
+        command_executor="http://localhost:4444/wd/hub",
+        options=options
+    )
+
+    yield driver
+
+    driver.quit()
---
+++ saucedemo-selenium-python/tests/test_saucedemo.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+
+def test_open_saucedemo(driver):
+
+    driver.get("https://www.saucedemo.com")
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    driver.save_screenshot(
+        f"screenshots/saucedemo_{timestamp}.png"
+    )
+
+    assert "Swag Labs" in driver.title
