9355c5ef22e0ec43527118ddf276a0173f049c8f
Hannah Cameron <77558582+laheara143@users.noreply.github.com>
2026-09-21 23:16:51 +0000

    Adding remove from cart script

--- saucedemo-selenium-python/tests/test_remove_from_cart.py
+++ saucedemo-selenium-python/tests/test_remove_from_cart.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 
-def test_valid_add_to_cart(driver):
+def test_remove_from_cart(driver):
 
     driver.get("https://www.saucedemo.com")
 
@@ -16,35 +16,18 @@ def test_valid_add_to_cart(driver):
     #Add Item
     driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
 
-    #Remove Item
-    driver.find_element(By.ID, "remove-sauce-labs-backpack" ).click()
-
-    driver.save_screenshot(f"screenshots/debug_saucedemo.png")
-
-    #Add Item back
-    driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
-
     #Verify cart Updates
     cart_badge = driver.find_element(By.CLASS_NAME, "shopping_cart_badge")   
     assert cart_badge.text == "1"
 
-    #Navigate to Cart
-    driver.find_element(By.ID, "shopping_cart_container").click()
-    driver.find_element(By.ID, "checkout").click()
+    #Remove Item
+    driver.find_element(By.ID, "remove-sauce-labs-backpack" ).click()
 
-    #Enter Checkout Info
-    driver.find_element(By.ID, "first-name").send_keys('standard')
-    driver.find_element(By.ID, "last-name").send_keys('user')
-    driver.find_element(By.ID, "postal-code").send_keys('01111')
+    driver.save_screenshot(f"1screenshots/debug_saucedemo.png")
 
-    #Click COntinue
-    driver.find_element(By.ID, "continue").click()
-
-    #Verify Total
-    assert total.endswith("32.39") 
-    
-    #Checkout
-    driver.find_element(By.ID, "finish").click()
+    #Verify cart Updates
+    cart_badge = driver.find_element(By.CLASS_NAME, "shopping_cart_badge")   
+    assert cart_badge.text == "1"
 
     #Verify With Screenshot
     driver.save_screenshot(f"screenshots/saucedemo_{timestamp}.png")
